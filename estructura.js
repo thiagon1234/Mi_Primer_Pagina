1fd7a6afb2122a0fcd0d02fc0a01650dadefb5da
Thiago Nuñez <thiagonunezlarrosa@gmail.com>
2025-05-25 15:30:59 -0300

    cambios

---
+++ estructura.js
@@ -0,0 +1,9 @@
+function mostrarPrecios() {
+  const precios = document.getElementById('precios');
+  precios.style.display = precios.style.display === 'none' ? 'block' : 'none';
+}
+
+function mostrarOfertas() {
+  const ofertas = document.getElementById('menuOfertas');
+  ofertas.style.display = ofertas.style.display === 'none' ? 'block' : 'none';
+}
